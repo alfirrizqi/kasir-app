b431e92256a04a0f258f7c34543e5d8a57c493a3
alfirrizqi <ivan_alfirrizqi@outlook.com>
2023-05-04 21:48:19 +0700

    layoutbarutapiblmjadi

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -1,5 +1,5 @@
 import Navbar from "../component/navbar";
-import { Grid, GridItem, Container, Box, Text, Button, Stack } from '@chakra-ui/react'
+import { Grid, GridItem, Container, Box, Text, Button, Stack, SimpleGrid } from '@chakra-ui/react'
 import MenuProduk from '../component/productList'
 // import ListCategories from '../component/category'
 
@@ -7,7 +7,33 @@ const MainMenu = () => {
   return (
     <div className="App">
       <Navbar />
-      
+      <SimpleGrid columns={3} spacing='500px' px='10px'>
+        <Box height='100%' bg='blue.200' mt='16' textAlign='center'>
+          <Text as='b' fontSize='3xl' mt='10' spacing='3' size='md'>Daftar Kategori</Text>
+          <Stack direction='row' spacing={4} mt='16' justifyContent='center'>
+            <Button colorScheme='teal' variant='outline' >
+             Makanan
+            </Button>
+            <Button colorScheme='teal' variant='outline'>
+              Minuman
+            </Button>
+          </Stack>
+        </Box>
+        <Box bg='blue.200' height='80px' mt='16' >
+        <MenuProduk />
+        </Box>
+        <Box bg='blue.200' height='80px' mt='16' textAlign='center'>
+        <Text as='b' fontSize='3xl' mt='10' spacing='3' >Product Config</Text>
+        <Stack direction='row' spacing={4} mt='16' justifyContent='center'>
+            <Button colorScheme='teal' variant='outline'>
+             Insert Product
+            </Button>
+            <Button colorScheme='teal' variant='outline'>
+              Edit Product
+            </Button>
+          </Stack>
+        </Box>
+      </SimpleGrid>
       {/* <Box position='relative' gap={12} p={12} columns={2} bgGradient="linear(to-r, red.300, blue.400, red.300)" >
         <Container>
           <Grid templateColumns='repeat(5, 1fr)' gap={4}>
