2ef3fa8ad4d7a138b00b5a51505babe4305c43ae
Rinaldy97 <rinaldysaputra77@gmail.com>
2023-05-08 22:52:59 +0700

    push

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import register from './pages/register';
 import MainMenu from './pages/productPage';
+import Insert from './component/category';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
     <Router>
       <Routes>
-        <Route path="/register" element={<register/>}/>
+        <Route path="/category" element={<Insert/>}/>
         <Route path="/productPage" element={<MainMenu/>}/>
       </Routes>
     </Router>
